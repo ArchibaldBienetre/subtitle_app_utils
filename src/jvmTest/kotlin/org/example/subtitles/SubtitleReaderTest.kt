e6f9fe1aee84beaef8ec326ee693ef39ecab06e4
Robert G <bluewhobert@mailbox.org>
2020-05-02 22:20:18 +0200

    extend SubtitleReaderTest

--- src/jvmTest/kotlin/org/example/subtitles/SubtitleReaderTest.kt
+++ src/jvmTest/kotlin/org/example/subtitles/SubtitleReaderTest.kt
@@ -73,5 +73,12 @@ class SubtitleReaderTest {
 
         val sequenceEntries = actual.take(10).toList()
         assertEquals(6, sequenceEntries.size)
+        val converter = SubtitleEntrySrtConverter()
+        assertEquals(converter.fromSrtString("1\n00:01:35,628 --> 00:01:36,654\nDégage, toi.\n\n"), sequenceEntries.get(0))
+        assertEquals(converter.fromSrtString("2\n00:01:58,209 --> 00:01:59,006\nLes voilà.\n\n"), sequenceEntries.get(1))
+        assertEquals(converter.fromSrtString("11\n00:02:50,209 --> 00:02:53,583\n- Je double : 200 sur l'escorte.\n- Vous allez perdre.\n\n"), sequenceEntries.get(2))
+        assertEquals(converter.fromSrtString("29\n00:03:28,574 --> 00:03:31,490\n- Je roule pas à 180 pour m'amuser !\n- Oh !\n\n"), sequenceEntries.get(3))
+        assertEquals(converter.fromSrtString("1370\n01:42:10,779 --> 01:42:12,963\n...je l'ai enfin retrouvé.\n\n"), sequenceEntries.get(4))
+//        assertEquals(converter.fromSrtString("1371\n01:42:14,312 --> 01:42:15,962\nEmbrassez-la pour moi.\n\n", sequenceEntries.get(5))
     }
 }
