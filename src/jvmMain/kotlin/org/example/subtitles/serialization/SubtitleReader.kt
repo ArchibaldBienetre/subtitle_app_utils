7b3266b4505e6771e5a7ccfe092a0c6381561e0c
Robert G <bluewhobert@mailbox.org>
2020-05-03 12:17:46 +0200

    SubtitleReader: provide exception handling + listener

--- src/jvmMain/kotlin/org/example/subtitles/serialization/SubtitleReader.kt
+++ src/jvmMain/kotlin/org/example/subtitles/serialization/SubtitleReader.kt
@@ -6,6 +6,7 @@ import java.io.BufferedReader
 import java.io.Closeable
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.util.function.Consumer
 import javax.annotation.WillClose
 
 class SubtitleReader(@WillClose inputStream: InputStream, private val converter: SubtitleEntryConverter) :
@@ -13,37 +14,41 @@ class SubtitleReader(@WillClose inputStream: InputStream, private val converter:
     private val underlyingInputStream: InputStream = inputStream
     private val bufferedReader: BufferedReader = BufferedReader(InputStreamReader(inputStream))
 
-    fun streamSubtitleEntries(): Sequence<SubtitleEntry> {
+    fun streamSubtitleEntries(errorHandler: Consumer<Exception> = Consumer { }): Sequence<SubtitleEntry> {
         val outerObject = this
         return sequence {
             outerObject.use {
                 while (bufferedReader.ready()) {
-                    readNextSubtitleEntry()?.also { yield(it) }
+                    readNextSubtitleEntry(errorHandler)?.also { yield(it) }
                 }
             }
         }
     }
 
-    private fun readNextSubtitleEntry(): SubtitleEntry? {
-        val currentEntryStrings: MutableList<String> = ArrayList()
-        var started = false
-        do {
-            val currentLine = bufferedReader.readLine()
+    private fun readNextSubtitleEntry(errorHandler: Consumer<Exception>): SubtitleEntry? {
+        try {
+            val currentEntryStrings: MutableList<String> = ArrayList()
+            var started = false
+            do {
+                val currentLine = bufferedReader.readLine()
 
-            // ignore leading empty lines
-            if (!started && !Strings.isNullOrEmpty(currentLine)) {
-                started = true
-            }
-            if (started) {
-                currentEntryStrings.add(currentLine)
-            }
-        } while (bufferedReader.ready() && !Strings.isNullOrEmpty(currentLine))
+                // ignore leading empty lines
+                if (!started && !Strings.isNullOrEmpty(currentLine)) {
+                    started = true
+                }
+                if (started) {
+                    currentEntryStrings.add(currentLine)
+                }
+            } while (bufferedReader.ready() && !Strings.isNullOrEmpty(currentLine))
 
-        if (currentEntryStrings.asSequence().all { Strings.isNullOrEmpty(it) }) {
-            return null
+            if (!currentEntryStrings.asSequence().all { Strings.isNullOrEmpty(it) }) {
+                val srtString = currentEntryStrings.joinToString("\n", postfix = "\n")
+                return converter.fromString(srtString)
+            }
+        } catch (e: Exception) {
+            errorHandler.accept(e)
         }
-        val srtString = currentEntryStrings.joinToString("\n", postfix = "\n")
-        return converter.fromString(srtString)
+        return null
     }
 
     override fun close() {
--- src/jvmTest/kotlin/org/example/subtitles/serialization/SubtitleReaderTest.kt
+++ src/jvmTest/kotlin/org/example/subtitles/serialization/SubtitleReaderTest.kt
@@ -6,8 +6,12 @@ import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileInputStream
+import java.lang.IllegalArgumentException
 import java.time.LocalTime
+import java.util.concurrent.atomic.AtomicReference
+import java.util.function.Consumer
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 class SubtitleReaderTest {
 
@@ -64,6 +68,24 @@ class SubtitleReaderTest {
         assertEquals(entry, sequenceEntries.get(0))
     }
 
+
+    @Test
+    fun readTestSubtitlesSingleEntry_readerror() {
+        val entriesStream = ByteArrayOutputStream()
+        entriesStream.writeBytes("intentionally illegal format".toByteArray())
+        val inputStream = ByteArrayInputStream(entriesStream.toByteArray())
+        val sut = SubtitleReader(inputStream, SubtitleEntrySrtConverter())
+        val caughtException: AtomicReference<Exception> = AtomicReference()
+        val errorHandler = Consumer(caughtException::set)
+
+        val actual = sut.streamSubtitleEntries(errorHandler)
+
+        val sequenceEntries = actual.take(1).toList()
+        assertEquals(0, sequenceEntries.size)
+        assertNotNull(caughtException.get())
+        assertEquals(IllegalArgumentException::class, caughtException.get()::class)
+    }
+
     @Test
     fun readTestSubtitlesFromFile() {
         val testFile = File("src/jvmTest/resources/test_subtitles.srt")
@@ -77,9 +99,21 @@ class SubtitleReaderTest {
         val converter = SubtitleEntrySrtConverter()
         assertEquals(converter.fromString("1\n00:01:35,628 --> 00:01:36,654\nDégage, toi.\n\n"), sequenceEntries.get(0))
         assertEquals(converter.fromString("2\n00:01:58,209 --> 00:01:59,006\nLes voilà.\n\n"), sequenceEntries.get(1))
-        assertEquals(converter.fromString("11\n00:02:50,209 --> 00:02:53,583\n- Je double : 200 sur l'escorte.\n- Vous allez perdre.\n\n"), sequenceEntries.get(2))
-        assertEquals(converter.fromString("29\n00:03:28,574 --> 00:03:31,490\n- Je roule pas à 180 pour m'amuser !\n- Oh !\n\n"), sequenceEntries.get(3))
-        assertEquals(converter.fromString("1370\n01:42:10,779 --> 01:42:12,963\n...je l'ai enfin retrouvé.\n\n"), sequenceEntries.get(4))
-        assertEquals(converter.fromString("1371\n01:42:14,312 --> 01:42:15,962\nEmbrassez-la pour moi.\n\n"), sequenceEntries.get(5))
+        assertEquals(
+            converter.fromString("11\n00:02:50,209 --> 00:02:53,583\n- Je double : 200 sur l'escorte.\n- Vous allez perdre.\n\n"),
+            sequenceEntries.get(2)
+        )
+        assertEquals(
+            converter.fromString("29\n00:03:28,574 --> 00:03:31,490\n- Je roule pas à 180 pour m'amuser !\n- Oh !\n\n"),
+            sequenceEntries.get(3)
+        )
+        assertEquals(
+            converter.fromString("1370\n01:42:10,779 --> 01:42:12,963\n...je l'ai enfin retrouvé.\n\n"),
+            sequenceEntries.get(4)
+        )
+        assertEquals(
+            converter.fromString("1371\n01:42:14,312 --> 01:42:15,962\nEmbrassez-la pour moi.\n\n"),
+            sequenceEntries.get(5)
+        )
     }
 }
