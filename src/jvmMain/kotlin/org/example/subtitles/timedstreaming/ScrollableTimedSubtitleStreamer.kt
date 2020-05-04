db20f4e809d190fbf5649759004faa4f469bc4a1
Robert G <bluewhobert@mailbox.org>
2020-05-04 17:51:58 +0200

    interfaces: add JavaDoc

--- src/jvmMain/kotlin/org/example/subtitles/timedstreaming/ScrollableTimedSubtitleStreamer.kt
+++ src/jvmMain/kotlin/org/example/subtitles/timedstreaming/ScrollableTimedSubtitleStreamer.kt
@@ -3,5 +3,9 @@ package org.example.subtitles.timedstreaming
 import java.time.LocalTime
 
 interface ScrollableTimedSubtitleStreamer : TimedSubtitleStreamer {
+
+    /**
+     * Set playback time to the given time.
+     */
     fun scrollToTimestamp(timestamp: LocalTime)
 }
--- src/jvmMain/kotlin/org/example/subtitles/timedstreaming/TimedSubtitleStreamer.kt
+++ src/jvmMain/kotlin/org/example/subtitles/timedstreaming/TimedSubtitleStreamer.kt
@@ -4,9 +4,19 @@ import java.io.Closeable
 
 interface TimedSubtitleStreamer : Closeable {
 
+    /**
+     * Start or continue streaming from the current playback time.
+     */
     fun startOrContinue()
 
+
+    /**
+     * Temporarily pause streaming. Will not affect playback time.
+     */
     fun pause()
 
+    /**
+     * Stop playback, reset playback time to the beginning.
+     */
     fun stop()
 }
