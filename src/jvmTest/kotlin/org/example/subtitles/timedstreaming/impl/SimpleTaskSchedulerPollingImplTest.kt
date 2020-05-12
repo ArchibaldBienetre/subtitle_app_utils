bb0f5436abf5ec63c60f1898ebc8860eb314fe25
Robert G <bluewhobert@mailbox.org>
2020-05-12 18:02:16 +0200

    add polling Thread-based implementation of SimpleTaskScheduler

---
+++ src/jvmTest/kotlin/org/example/subtitles/timedstreaming/impl/SimpleTaskSchedulerPollingImplTest.kt
@@ -0,0 +1,85 @@
+package org.example.subtitles.timedstreaming.impl
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit.SECONDS
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class SimpleTaskSchedulerPollingImplTest {
+
+    private lateinit var sut: SimpleTaskSchedulerPollingImpl
+
+    @Before
+    fun setUp() {
+        sut = SimpleTaskSchedulerPollingImpl()
+    }
+
+    @After
+    fun tearDown() {
+        sut.close()
+    }
+
+    @Test
+    fun scheduleAtMillisFromNow() {
+        val latch = CountDownLatch(1)
+
+        sut.scheduleAtMillisFromNow(2000) {
+            latch.countDown()
+        }
+
+        val wasExecuted = latch.await(3, SECONDS)
+        assertTrue(wasExecuted)
+    }
+
+    @Test
+    fun scheduleAtMillisFromNow_multiple() {
+        val latch1 = CountDownLatch(1)
+        val latch2 = CountDownLatch(1)
+        val latch3 = CountDownLatch(1)
+
+        sut.scheduleAtMillisFromNow(4900) {
+            latch1.countDown()
+        }
+        sut.scheduleAtMillisFromNow(4000) {
+            latch2.countDown()
+        }
+        sut.scheduleAtMillisFromNow(2000) {
+            latch3.countDown()
+        }
+
+        val wasExecuted1 = latch1.await(5, SECONDS)
+        val wasExecuted2 = latch2.await(1, SECONDS)
+        val wasExecuted3 = latch3.await(1, SECONDS)
+        assertTrue(wasExecuted1)
+        assertTrue(wasExecuted2)
+        assertTrue(wasExecuted3)
+    }
+
+    @Test
+    fun cancelAllScheduled() {
+        val latch1 = CountDownLatch(1)
+        val latch2 = CountDownLatch(1)
+        val latch3 = CountDownLatch(1)
+        sut.scheduleAtMillisFromNow(5000) {
+            latch1.countDown()
+        }
+        sut.scheduleAtMillisFromNow(4000) {
+            latch2.countDown()
+        }
+        sut.scheduleAtMillisFromNow(2000) {
+            latch3.countDown()
+        }
+
+        sut.cancelAllScheduled()
+
+        val wasExecuted1 = latch1.await(5, SECONDS)
+        val wasExecuted2 = latch2.await(1, SECONDS)
+        val wasExecuted3 = latch3.await(1, SECONDS)
+        assertFalse(wasExecuted1)
+        assertFalse(wasExecuted2)
+        assertFalse(wasExecuted3)
+    }
+}
