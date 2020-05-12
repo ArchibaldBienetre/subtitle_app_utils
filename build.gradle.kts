60c96a8ac2fdd7a1b48a832ce1a0487c4c7a54d5
Robert G <bluewhobert@mailbox.org>
2020-05-12 21:04:45 +0200

    stepwise transformation to Kotlin DSL - part 2
    * repositories {...} statement
    * some variables
    * applying plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,19 @@ plugins {
     id("org.jetbrains.kotlin.multiplatform") version "1.3.72"
 }
 
+repositories {
+    mavenCentral()
+
+    maven {
+        url = project.uri("https://dl.bintray.com/mockito/maven")
+    }
+}
+
+group = "com.example"
+version = "1.0.1-SNAPSHOT"
+
+apply(plugin = "maven-publish")
+apply(plugin = "jacoco")
+
 apply(from = "other.gradle")
 
