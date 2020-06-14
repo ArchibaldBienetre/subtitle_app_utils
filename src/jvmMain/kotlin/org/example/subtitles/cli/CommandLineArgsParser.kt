2d9c915f2cba65d963ca4ea76b36dbeb202860ca
Robert G <bluewhobert@mailbox.org>
2020-06-14 14:34:06 +0200

    change parameter to Duration - negative values must be allowed

--- src/jvmMain/kotlin/org/example/subtitles/cli/CommandLineArgsParser.kt
+++ src/jvmMain/kotlin/org/example/subtitles/cli/CommandLineArgsParser.kt
@@ -2,6 +2,7 @@ package org.example.subtitles.cli
 
 import java.io.File
 import java.io.PrintWriter
+import java.time.Duration
 import java.time.LocalTime
 
 sealed class BasicCommandLineParams(open val inputFile: File)
@@ -12,7 +13,7 @@ data class StreamingCommandLineParams(
 
 data class ModificationCommandLineParams(
     override val inputFile: File,
-    val modificationOffset: LocalTime,
+    val modificationOffset: Duration,
     val outputFile: File
 ) : BasicCommandLineParams(inputFile = inputFile)
 
